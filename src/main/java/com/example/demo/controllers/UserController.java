324867881c745fe7c270a95ba61dc9b70c81af6f
Roberto <robertodefreitas@users.noreply.github.com>
2021-12-09 19:20:59 +0100

    Add the Logger dependency and first configuration

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,7 +23,13 @@ import org.springframework.security.crypto.bcrypt.BCrypt;
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-	
+
+	/**
+	 * From video ND035 C04 L03 A05
+	 * P4-L2-10
+	 */
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
 	@Autowired
 	private UserRepository userRepository;
 	
@@ -53,6 +61,8 @@ public class UserController {
 	public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
 		User user = new User();
 		user.setUsername(createUserRequest.getUsername());
+		log.info("User name set with " + createUserRequest.getUsername());
+
 		Cart cart = new Cart();
 		cartRepository.save(cart);
 		user.setCart(cart);
